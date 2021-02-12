2f5cf1feb15a2d6e51129506dc0a1eb25b77614c
ujjayini08 <upoddar74@gmail.com>
2021-02-12 15:51:14 +0530

    my first firebase

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
-var ball;
+var ball, database,position;
 
 function setup(){
+
+    database= firebase.database();
+
     createCanvas(500,500);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPosition=database.ref("ball/position");
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        "x":position.x+x,
+        "y":position.y+y
+    })  
+}
+function readPosition(data)
+{
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showError()
+{
+    console.log("Error in writing to the database");
 }
